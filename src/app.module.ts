acbbec752a0f88a09f53d6649a819aac5405f249
Ivan Yarovyi <trollbot2004@gmail.com>
2026-08-01 23:31:54 +0300

    Add structured logging with nestjs-pino

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { envValidationSchema } from './config/env.validation';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -11,6 +12,25 @@ import { envValidationSchema } from './config/env.validation';
       validationOptions: {
         abortEarly: false,
       },
+    }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        pinoHttp: {
+          level: config.get<string>('LOG_LEVEL') || 'info',
+          transport:
+            process.env.NODE_ENV !== 'production'
+              ? {
+                  target: 'pino-pretty',
+                  options: {
+                    colorize: true,
+                    singleLine: true,
+                    translateTime: 'HH:MM:ss',
+                  },
+                }
+              : undefined,
+        },
+      }),
     }),
   ],
   controllers: [AppController],
