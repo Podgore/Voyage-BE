10718128721d3919aa1d75300fe4a41d92cad9e1
Ivan Yarovyi <trollbot2004@gmail.com>
2026-08-11 01:19:13 +0300

    Fix PrismaPg adapter initialization and tested RBAC controller

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
       .setDescription('Voyage backend API documentation')
       .setVersion('1.0')
       .addTag('Health')
+      .addBearerAuth()
       .build();
 
     const documentFactory = () => SwaggerModule.createDocument(app, config);
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,14 +2,15 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
     super({ adapter });
   }
 
