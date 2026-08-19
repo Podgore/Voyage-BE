b327c2d5ca2b8ed35f1d1127c6e49b8f2c1f37cb
Ivan Yarovyi <trollbot2004@gmail.com>
2026-08-19 12:04:01 +0300

    add missing env variables for e2e tests

--- test/e2e-env.ts
+++ test/e2e-env.ts
@@ -2,3 +2,6 @@ process.env.JWT_ACCESS_SECRET ??= 'e2e-access-secret';
 process.env.JWT_REFRESH_SECRET ??= 'e2e-refresh-secret';
 process.env.JWT_ACCESS_EXPIRES_IN ??= '15m';
 process.env.JWT_REFRESH_EXPIRES_IN ??= '7d';
+process.env.ORIGIN ??= 'http://localhost:5173';
+process.env.THROTTLE_TTL ??= '60000';
+process.env.THROTTLE_LIMIT ??= '20';
