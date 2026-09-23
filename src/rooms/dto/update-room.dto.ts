c34bef51371a76d2cb9798a2483531915720355c
Ivan Yarovyi <trollbot2004@gmail.com>
2026-09-23 11:11:58 +0300

    separate constraint class from update-room.dto

--- src/rooms/dto/update-room.dto.ts
+++ src/rooms/dto/update-room.dto.ts
@@ -4,23 +4,9 @@ import {
   MinLength,
   Validate,
   ValidateIf,
-  ValidationArguments,
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
 } from 'class-validator';
 
-@ValidatorConstraint({ name: 'hasRoomUpdate', async: false })
-class HasRoomUpdateConstraint implements ValidatorConstraintInterface {
-  validate(_: unknown, args: ValidationArguments): boolean {
-    const request = args.object as UpdateRoomDto;
-
-    return request.name !== undefined || request.regenerateInviteCode === true;
-  }
-
-  defaultMessage(): string {
-    return 'Provide a name or set regenerateInviteCode to true';
-  }
-}
+import { HasRoomUpdateConstraint } from './has-room-update.constraint';
 
 export class UpdateRoomDto {
   @ValidateIf((_, value) => value !== undefined)
